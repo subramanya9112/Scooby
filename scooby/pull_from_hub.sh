92b1335ea5159daa0f469949d285298bc27e9404
Subramanya G <subramanyag9112@gmail.com>
2022-07-06 01:57:49 +0530

    Changed the url

---
+++ scooby/pull_from_hub.sh
@@ -0,0 +1,4 @@
+docker image pull subramanyag/server:latest
+docker image pull subramanyag/client:latest
+docker image pull subramanyag/game_server:latest
+docker image tag subramanyag/game_server:latest game_server
