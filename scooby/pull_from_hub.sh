7f2b3b28913191ebd5a368aba144df1b476ac912
Subramanya G <subramanyag9112@gmail.com>
2022-07-26 08:36:19 +0530

    Few changes

--- scooby/pull_from_hub.sh
+++ scooby/pull_from_hub.sh
@@ -1,5 +1,5 @@
 docker image pull subramanyag/server:latest
 docker image pull subramanyag/client:latest
 docker image pull subramanyag/game_server:latest
-docker image tag subramanyag/visualizer:latest
+docker image pull subramanyag/visualizer:latest
 docker image tag subramanyag/game_server:latest game_server
